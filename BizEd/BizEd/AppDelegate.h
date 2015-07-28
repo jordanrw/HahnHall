33d5f1466d64e1bf5e220dcb4f0de1cf5fc89817
Jordan White <jordanrw@vt.edu>
2015-07-28 13:15:32 -0700

    initial commit

---
+++ BizEd/BizEd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BizEd
+//
+//  Created by Jordan White on 7/28/15.
+//  Copyright (c) 2015 Two Beards and Fro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
